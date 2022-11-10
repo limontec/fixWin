08a331740676bf13ace0a4f285ccd3691242a61a
Limon <58980448+limontec@users.noreply.github.com>
2022-11-10 18:24:41 -0300

    Update fixWin.bat
    add suporte liberar espaço em disco
    add suporte criação de ponto de restauração automático

--- fixWin.bat
+++ fixWin.bat
@@ -5,6 +5,11 @@ ECHO Por favor espere... Verificando seu sistema.
 :: Section 1: Verificando arquivos do sistema para reparar arquivos de sistema ausentes ou corrompidos
 sfc /scannow
 DISM /Online /Cleanup-image /Restorehealth
+:: Section 2: Libera espaço ao limpar componentes atualizados. Tempo limite de 1h
+DISM /online /Cleanup-Image /StartComponentCleanup
+:: Section 3: Cria ponto de restauração automático
+ECHO Tentando criar ponto de restauração automatico para voce.
+wmic /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Restore Point", 100, 7
 ECHO ==================================================================================
 ECHO Processo finalizado, as mensagens acima informa o sucesso ou nao no procedimento.
 ECHO ==================================================================================
